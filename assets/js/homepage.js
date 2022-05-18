fa542094c93199add0982794eba2ea92d61bd12b
Olivia Kim <kimhaejeong2525@gmail.com>
2022-05-18 17:50:38 -0400

    api working now

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -5,24 +5,27 @@ var currentWeather=document.querySelector("#current-weather");
 
 var formSubmitHandler= function(event){
     event.preventDefault();
+    console.log("myname")
     var cityname =citySearch.value.trim();
 
     if(cityname){
-        getCityInfo(cityname);
-
         currentWeather.textContent="";
+        getLocation(cityname);
     } else {
         alert("Please enter a city name");
     }
 };
 
-function getLocation() {
-    var apiUrl="https://api.openweathermap.org/data/2.5/onecall?lat="+lat + "&lon=" + lon + "&appid=f0848502da03f1a49e19e44164dd9d9c";
+function getLocation(data) {
+    var apiUrl="http://api.openweathermap.org/geo/1.0/direct?q=" + data + "&limit=5&appid=f0848502da03f1a49e19e44164dd9d9c"
     fetch(apiUrl).then(function(response){
         if(response.ok){
             response.json().then(function(data){
-                var lat = data[0].latitude;
-                var lon = data[0].longitude;
+                var lat = data[0].lat;
+                var lon = data[0].lon;
+                console.log("latitude: "+ lat);
+                console.log(lon);
+                getWeather(lat,lon);
             });
         };
     }).catch(function(error){
@@ -37,6 +40,7 @@ function getWeather(lat,lon) {
             response.json().then(function(data){
                 
                 displayWeather();
+                console.log(data);
 
             })
         }
@@ -49,4 +53,4 @@ function displayWeather () {
 
 }
 
-searchFormEl.addEventListener("submit",formSubmitHandler);
+searchFormEl.addEventListener("click",formSubmitHandler);
