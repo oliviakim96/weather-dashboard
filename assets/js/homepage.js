475968a4d9ba819cdcb445839520067e45f3122e
Olivia Kim <kimhaejeong2525@gmail.com>
2022-05-18 14:09:55 -0400

    apicall

---
+++ assets/js/homepage.js
@@ -0,0 +1,46 @@
+var citySearch=document.querySelector("#city-search");
+var searchHistory=document.querySelector("#search-history");
+var searchFormEl= document.querySelector("#search-form");
+var currentWeather=document.querySelector("#current-weather");
+
+var formSubmitHandler= function(event){
+    event.preventDefault();
+    var cityname =citySearch.value.trim();
+
+    if(cityname){
+        getCityInfo(cityname);
+
+        currentWeather.textContent="";
+    } else {
+        alert("Please enter a city name");
+    }
+};
+
+function getLocation() {
+    var apiUrl=`https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=f0848502da03f1a49e19e44164dd9d9c`
+    fetch(apiUrl).then(function(response){
+        if(response.ok){
+            response.json().then(function(data){
+                var lat = data[0].latitude;
+                var lon = data[0].longitude;
+            });
+        };
+    }).catch(function(error){
+        console.log(error)
+    });
+};
+
+function getWeather(lat,lon) {
+    var apiUrl=`https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=f0848502da03f1a49e19e44164dd9d9c`
+    fetch(apiUrl).then(function(response){
+        if(response.ok){
+            response.json().then(function(data){
+                
+
+
+            })
+        }
+    }).catch(function(error){
+        console.log(error)
+    })
+};
